93a151e96eb3dafbf9e88e128dc938dec98362be
Aidan <cookar4@appstate.edu>
2023-03-27 13:51:37 -0400

    Implemented MemoryStage setInput

--- MemoryStage.C
+++ MemoryStage.C
@@ -14,12 +14,37 @@
 
 bool MemoryStage::doClockLow(PipeReg ** pregs, Stage ** stages)
 {
+   M * mreg = (M *) pregs[MREG];
+   W * wreg = (W *) pregs[WREG];
 
+   setWInput(wreg, 0, mreg->geticode()->getOutput(), mreg->getvalE()->getOutput(),
+   	0, mreg->getdstE()->getOutput(), mreg->getdstM()->getOutput());
+   return false;
 }
 
 
 
 void MemoryStage::doClockHigh(PipeReg ** pregs)
 {
+   M * mreg = (M *) pregs[MREG];
+   W * wreg = (W *) pregs[WREG];
 
+   wreg->getstat()->normal();
+   wreg->geticode()->normal();
+   wreg->getvalE()->normal();
+   wreg->getvalM()->normal();
+   wreg->getdstE()->normal();
+   wreg->getdstM()->normal();
+
+}
+
+void MemoryStage::setWInput(W * wreg, uint64_t stat, uint64_t icode, uint64_t valE, uint64_t valM,
+	uint64_t dstE, uint64_t dstM)
+{
+   wreg->getstat()->setInput(stat);
+   wreg->geticode()->setInput(icode);
+   wreg->getvalE()->setInput(valE);
+   wreg->getvalM()->setInput(valM);
+   wreg->getdstE()->setInput(dstE);
+   wreg->getdstM()->setInput(dstM);
 }
