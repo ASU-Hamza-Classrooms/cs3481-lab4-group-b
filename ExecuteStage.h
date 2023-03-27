85bb957afe5d5c02d672eee069db1edbf50e951a
Chris-Cole-1 <hartcj2@appstate.edu>
2023-03-27 13:35:43 -0400

    fixed ExecuteStage.h

--- ExecuteStage.h
+++ ExecuteStage.h
@@ -1,10 +1,9 @@
-
 class ExecuteStage: public Stage
 {
    private:
-      void setMInput(M * dreg, uint64_t stat, uint64_t icode, uint64_t ifun, 
-                     uint64_t rA, uint64_t rB,
-                     uint64_t valC, uint64_t valP);
+      void setMInput(M * mreg, uint64_t stat, uint64_t icode, uint64_t Cnd, 
+                     uint64_t valE, uint64_t valA,
+                     uint64_t dstE, uint64_t dstM);
    public:
       bool doClockLow(PipeReg ** pregs, Stage ** stages);
       void doClockHigh(PipeReg ** pregs);
