1783a061501c2a3d4bf930bfabc72f2ea97b5a87
Chris-Cole-1 <hartcj2@appstate.edu>
2023-03-27 13:42:39 -0400

    fixed MemoryStage.h file

--- MemoryStage.h
+++ MemoryStage.h
@@ -1,10 +1,9 @@
-
 class MemoryStage: public Stage
 {
    private:
-      void setWInput(W * dreg, uint64_t stat, uint64_t icode, uint64_t ifun, 
-                     uint64_t rA, uint64_t rB,
-                     uint64_t valC, uint64_t valP);
+      void setWInput(W * wreg, uint64_t stat, uint64_t icode, uint64_t valE, 
+                     uint64_t valM, uint64_t dstE,
+                     uint64_t dstM);
    public:
       bool doClockLow(PipeReg ** pregs, Stage ** stages);
       void doClockHigh(PipeReg ** pregs);
