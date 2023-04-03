0e07ddc21ef2cf9fd5d3ca91a7852a49cb3b4132
Chris-Cole-1 <hartcj2@appstate.edu>
2023-04-03 10:54:57 -0400

    copied new run.sh for lab7

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 rm -f -r Outputs
 mkdir Outputs
 chmod g+rwx Outputs
-dir="/u/classes/3481/Spring2022/lab6/Tests"
+dir="/u/classes/3481/Spring2022/lab7/Tests"
 tests=( error1 error2 error3 error4 error5 error6
         error7 error8 error9 error10 error11 error12 
-        error13 error14 error15 error16 nop )
+        error13 error14 error15 error16 nop irmovq )
 
 numTests=0
 numPasses=0
