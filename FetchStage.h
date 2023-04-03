49e2f3f3141c4e6779ba9fccbf0aec5de8404315
Aidan <cookar4@appstate.edu>
2023-04-03 11:16:08 -0400

    Added getRegIds and buildValC

--- FetchStage.h
+++ FetchStage.h
@@ -14,5 +14,7 @@ class FetchStage: public Stage
       bool needValC(uint64_t f_icode);
       uint64_t predictPC(uint64_t f_icode, uint64_t f_valC, uint64_t f_valP);
       uint64_t PCincrement(uint64_t f_pc, bool needRegIds, bool needValC);
+      void getRegIds();
+      uint64_t buildValC();
 
 };
