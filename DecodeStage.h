530a77bc30696cfc74126d649c3630b288343fa0
Chris-Cole-1 <hartcj2@appstate.edu>
2023-04-03 11:51:31 -0400

    added new methods to h file

--- DecodeStage.h
+++ DecodeStage.h
@@ -8,5 +8,11 @@ class DecodeStage: public Stage
    public:
       bool doClockLow(PipeReg ** pregs, Stage ** stages);
       void doClockHigh(PipeReg ** pregs);
+      uint64_t getsrcA(uint64_t icode, uint64_t d_rA);
+      uint64_t getsrcB(uint64_t icode, uint64_t d_rB);
+      uint64_t getdstE(uint64_t icode, uint64_t d_rB);
+      uint64_t getdstM(uint64_t icode, uint64_t d_rA);
+      uint64_t selFwdA(D * dreg);
+      uint64_t fwdB(D * dreg);
 
 };
