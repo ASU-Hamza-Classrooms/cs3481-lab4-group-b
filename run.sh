916fd4b904f59584d8ea9d1941d360b033215efe
Chris-Cole-1 <hartcj2@appstate.edu>
2023-04-26 16:31:31 -0400

    created lab10 branch

--- run.sh
+++ run.sh
@@ -3,14 +3,18 @@
 rm -f -r Outputs
 mkdir Outputs
 chmod g+rwx Outputs
-dir="/u/css/classes/3481/Spring2022/lab9/Tests"
+dir="/u/css/classes/3481/Spring2022/lab10/Tests"
+
 tests=( error1 error2 error3 error4 error5 error6
-        error7 error8 error9 error10 error11 error12 nop irmovq 
+        error7 error8 error9 error10 error11 error12 
+        error13 error14 nop irmovq 
         addq andq subq xorq addqfor1 addqfor2 addqfor3
         addqfor4 addqfor5 addqfor6 rrmovq cmovle cmovl
         cmove cmovne cmovge cmovg mrmovq1 mrmovq2
         pushq1 pushq2 popq1 popq2 pushqpopq rmmovq
-        memfor1 memfor2
+        memfor1 memfor2 except1 except2 except3 except4 except5 except6
+        except7 loaduse1 loaduse2 jmp je jg jge jl jle jne
+        addOne sum jecall
       )
 
 numTests=0
