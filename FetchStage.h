47346d4bbcbacd57b971027467fb56862761670f
Aidan <cookar4@appstate.edu>
2023-05-01 10:52:06 -0400

    Added method prototypes to header

--- FetchStage.h
+++ FetchStage.h
@@ -16,5 +16,6 @@ class FetchStage: public Stage
       uint64_t PCincrement(uint64_t f_pc, bool needRegIds, bool needValC);
       void getRegIds(uint8_t instByte, uint64_t &rA, uint64_t &rB, uint64_t f_pc);
       uint64_t buildValC(uint64_t f_pc);
-
+      bool instr_valid(uint64_t f_icode);
+      uint64_t f_stat(bool mem_error, uint64_t f_icode);
 };
